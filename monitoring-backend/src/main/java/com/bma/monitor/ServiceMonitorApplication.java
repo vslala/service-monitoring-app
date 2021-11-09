6dc7dca9c29174fc4d56b29ec8dc4a4213a048ee
vslala <varunshrivastava007@gmail.com>
2021-11-10 00:50:48 +0100

    initial setup

---
+++ monitoring-backend/src/main/java/com/bma/monitor/ServiceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.bma.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceMonitorApplication.class, args);
+	}
+
+}
---
+++ monitoring-backend/src/test/java/com/bma/monitor/ServiceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bma.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
