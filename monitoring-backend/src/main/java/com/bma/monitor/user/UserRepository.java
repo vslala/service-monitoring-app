f51b814252149480e10822ee69f0270928bbc5f5
vslala <varunshrivastava007@gmail.com>
2021-11-11 00:51:40 +0100

    enable cross-origin resource sharing and expose location header

--- monitoring-backend/src/main/java/com/bma/monitor/user/UserRepository.java
+++ monitoring-backend/src/main/java/com/bma/monitor/user/UserRepository.java
@@ -1,8 +1,12 @@
 package com.bma.monitor.user;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.Optional;
+
+@CrossOrigin(value = "http://localhost:3000", exposedHeaders = {"Location"})
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     User findByUsername(String username);
