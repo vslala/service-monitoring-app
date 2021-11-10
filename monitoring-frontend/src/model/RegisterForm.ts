d4e2d0b863d5ac51fe0d9cdcdaabaf7be4e33f5f
vslala <varunshrivastava007@gmail.com>
2021-11-11 00:45:53 +0100

    add UserLoginForm and RegisterForm model

---
+++ monitoring-frontend/src/model/RegisterForm.ts
@@ -0,0 +1,6 @@
+interface RegisterForm {
+    username: string
+    password: string
+}
+
+export default RegisterForm
---
+++ monitoring-frontend/src/model/UserLoginForm.ts
@@ -0,0 +1,6 @@
+interface UserLoginForm {
+    username: string
+    password: string
+}
+
+export default UserLoginForm
