8c1f4cb6561e9f011755bc48f519a651f63096de
vslala <varunshrivastava007@gmail.com>
2021-11-11 23:10:37 +0100

    modify webservice model to add health history field and a list wrapper model

--- monitoring-frontend/src/model/WebService.ts
+++ monitoring-frontend/src/model/WebService.ts
@@ -1,9 +1,16 @@
+import {HealthStatus} from "./HealthHistory";
+
 interface WebService {
-    id:number
-    name:string
-    url:string
-    createdAt:string;
-    healthStatus:string
+    id: number
+    name: string
+    url: string
+    createdAt: string;
+    healthHistory:HealthStatus[]
+    _links: any
+}
+
+export interface WebServices {
+    services: WebService[]
 }
 
 export default WebService;
