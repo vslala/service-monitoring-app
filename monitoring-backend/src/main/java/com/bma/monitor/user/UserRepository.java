55563874276bf81129a55a201bb0ea6ceec8b44f
vslala <varunshrivastava007@gmail.com>
2021-11-11 22:58:40 +0100

    make the return type optional

--- monitoring-backend/src/main/java/com/bma/monitor/user/UserRepository.java
+++ monitoring-backend/src/main/java/com/bma/monitor/user/UserRepository.java
@@ -1,6 +1,6 @@
 package com.bma.monitor.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,5 +9,5 @@ import java.util.Optional;
 @CrossOrigin(value = "http://localhost:3000", exposedHeaders = {"Location"})
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
