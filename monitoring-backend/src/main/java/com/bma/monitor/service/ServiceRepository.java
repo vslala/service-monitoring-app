cf3785b5b2da2134f97a3fa02befe518001e5e56
vslala <varunshrivastava007@gmail.com>
2021-11-11 22:59:49 +0100

    support cross-origin resource sharing for Service

--- monitoring-backend/src/main/java/com/bma/monitor/service/ServiceRepository.java
+++ monitoring-backend/src/main/java/com/bma/monitor/service/ServiceRepository.java
@@ -2,7 +2,9 @@ package com.bma.monitor.service;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(value = "http://localhost:3000", exposedHeaders = {"Location"})
 @RepositoryRestResource(collectionResourceRel = "services", path = "services")
 public interface ServiceRepository extends PagingAndSortingRepository<Service, Long> {
     Service findByUserId(Long userId);
